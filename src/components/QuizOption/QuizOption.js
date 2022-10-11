3371ff2269cd9e1a4c3a4f9223c09207359f4dd7
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-10-11 21:08:44 +0600

    Quiz correct Ans slow in toast

--- src/components/QuizOption/QuizOption.js
+++ src/components/QuizOption/QuizOption.js
@@ -1,21 +1,24 @@
 import React from 'react';
 
 const QuizOption = ({ option }) => {
-    console.log(option)
+    // console.log('C', option.correctAnswer)
+    const checkOption = (option) => {
+        console.log(option);
+    }
     return (
         <div>
             <div>
-                <div>
+                {/* <div>
                     <input
                         type="radio"
                         name="answer"
                         value={option}
                         className="ml-5 dark:bg-gray-800"
                     />
-                    <label className="text-gray-700 dark:text-gray-400 text-lg ml-4">
+                    <label onClick={() => checkOption(option)} className="text-gray-700 dark:text-gray-400 text-lg ml-4">
                         {option}
                     </label>
-                </div>
+                </div> */}
             </div>
         </div>
     );
